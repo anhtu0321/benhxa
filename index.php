7c313208400c2977599b7f4d6d78e763ec5fa7d1
anhtu0321 <vantu9486@gmail.com>
2020-10-11 21:35:02 +0700

    home

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
-<?php 
-session_start();
-include("admin/config.php");
-$sql = "select id, tenmuctin, thutu from muctin where trangthai = 1 order by thutu ASC";
-$tbmuctin = mysqli_query($con,$sql);
-$mangmuctin = array();
-while($rsmuctin = mysqli_fetch_array($tbmuctin)){
-    $mangmuctin[] = array('id' => $rsmuctin["id"], 'tenmuctin' => $rsmuctin["tenmuctin"], 'thutu' => $rsmuctin["thutu"]); 
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Website nội bộ Công an tỉnh Hưng Yên</title>
+    <title>PHẦN MỀM QUẢN LÝ HỒ SƠ BỆNH NHÂN</title>
 </head>
 <link rel="stylesheet" type="text/css" href="style/bootstrap341/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="style/mystyle.css">
 <body>
-    
-        <div class="container wraper">
-            <?php include("include/banner.php");?>
-            <?php include("include/menu.php");?>
-            <?php include("include/main.php");?>
+    <div class="container-fluid top">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-7 text-left">
+                    <i class="glyphicon glyphicon-grain"></i> Bệnh xá Công an tỉnh Hưng Yên
+                </div>
+                <div class="col-xs-5 text-right">
+                <i class="glyphicon glyphicon-phone-alt"></i> SĐT: 069 2849 273
+                </div>
+            </div>
         </div>
-            <div class="container-fluid">
-        <?php include("include/footer.php");?>
-</div>
- 
+    </div>
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-7 text-left">
+                    <i class="glyphicon glyphicon-grain"></i> Bệnh xá Công an tỉnh Hưng Yên
+                </div>
+                <div class="col-xs-5 text-right">
+                <i class="glyphicon glyphicon-phone-alt"></i> SĐT: 069 2849 273
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 <script src="style/bootstrap341/js/jquery.js"></script>
 <script src="style/bootstrap341/js/popper.min.js"></script>
